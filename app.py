08ad50c225f8b31739f97b5e34f887e3661160a6
saisreeramh-web <saisreeramh@gmail.com>
2025-08-29 19:39:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-/upload
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Server is running ✅"
+
 @app.route("/upload", methods=["POST"])
 def upload():
-    return {"message": "File received!"}
+    return jsonify({"status": "ok", "message": "Audio received!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
