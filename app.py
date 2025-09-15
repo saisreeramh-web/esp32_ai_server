7214f50611156703b4f3ad30ce3436f74672e95c
saisreeramh-web <saisreeramh@gmail.com>
2025-09-15 18:00:29 +0530

    app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('uploading', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     data = request.data
     print("Received data length:", len(data))
-    return jsonify({"response": "Hello from the server!"})
+    return jsonify({"response": "Hello! I got your audio."})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
